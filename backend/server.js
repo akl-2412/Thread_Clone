e3dacc109e909efde8bd818de9b55b51bc8c2a03
akl-2412 <anuraglal2412@gmail.com>
2024-04-03 21:33:49 +0530

    Update Profile and Follow and Unfollow

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,24 @@ import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
-
+import { v2 as cloudinary } from "cloudinary";
+import bodyParser from "body-parser";
 dotenv.config();
 
 connectDB();
 const app = express();
-
+//const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 // Middlewares
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json()); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
---
+++ frontend/src/hooks/usePreviewImg.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import useShowToast from "./useShowToast";
+
+const usePreviewImg = () => {
+	const [imgUrl, setImgUrl] = useState(null);
+	const showToast = useShowToast();
+	const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		if (file && file.type.startsWith("image/")) {
+			const reader = new FileReader();
+           // console.log(reader);
+			reader.onloadend = () => {
+				setImgUrl(reader.result);
+                console.log(reader.result);
+			};
+            //console.log(imgUrl);
+			reader.readAsDataURL(file);
+		} else {
+			showToast("Invalid file type", " Please select an image file", "error");
+			setImgUrl(null);
+		}
+	};
+	return { handleImageChange, imgUrl };
+};
+
+export default usePreviewImg;
